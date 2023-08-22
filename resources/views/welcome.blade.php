b203d983c6f0241d2d3640c8d9788c6069b07c42
Bernard Antwi <bernardantwi2010@gmail.com>
2023-08-22 10:57:00 +0000

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,7 +122,7 @@
                     </div>
                 </div>
 
-                <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
+                {{-- <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                         <div class="flex items-center gap-4">
                             <a href="https://github.com/sponsors/jeremykenedy" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
@@ -132,7 +132,7 @@
                                 Sponsor
                             </a>
                         </div>
-                    </div>
+                    </div> --}}
 
                     {{-- <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) ({{ trans('titles.auth') }} v{{ config('settings.app_project_version') }})
